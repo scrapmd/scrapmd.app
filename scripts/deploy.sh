ed2d9b45231a563c1bc2a148be8599834f2cbf34
Atsushi Nagase <a@ngs.io>
2020-06-15 10:17:57 +0900

    Invalidate after deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ set -eux
 
 aws s3 sync --acl public-read public s3://scrapmd-app-website
 aws s3 cp --acl public-read --content-type application/json public/.well-known/apple-app-site-association s3://scrapmd-app-website/docs/.well-known/apple-app-site-association
-
+aws cloudfront create-invalidation --distribution-id $CLOUD_FRONT_DISTRIBUTION_ID --paths '/*'
